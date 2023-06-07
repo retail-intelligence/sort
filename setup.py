a35754f404d0470909d02b1cea6306d1648a071d
Filipe Bisinella <filipe1.bs@gmail.com>
2023-06-06 21:00:26 -0300

    fix import

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='sort',
       version='1.0',
       description='Retail Intelligence sort',
       author='nautec',
-      packages=find_packages(),
+      # packages=find_packages(),
       url='https://github.com/retail-intelligence/sort',
+      py_modules=["sort"],
     )
